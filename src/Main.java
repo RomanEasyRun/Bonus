e71b01c02731e0ebf959040db38eecf63b2140d9
RomanEasyRun <78853235+RomanEasyRun@users.noreply.github.com>
2021-03-19 20:49:34 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,13 @@ public class Main {
     public static void main(String[] args) {
         int balance = 30;
         int transfer = 1400;
-        int conditionOne = 100;
-        int conditionTwo = 1000;
-        int bonus = transfer / conditionOne;
-        int total = balance + transfer + bonus;
-        if (transfer > conditionTwo) {
-            transfer = conditionTwo;
+        int condition = 1000;
+        int bonus = transfer / 100;
+        if (transfer >= condition) {
+        } else {
+            bonus = 0;
         }
+        int total = balance + transfer + bonus;
         System.out.println(bonus);
         System.out.println(total);
     }
