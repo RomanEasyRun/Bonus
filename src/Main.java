5956ecb2042ef0ed0d4ea32e9f7f9b97699532d0
RomanEasyRun <78853235+RomanEasyRun@users.noreply.github.com>
2021-03-21 14:11:46 +0700

    Update Main.java
    Добавлено значение в if

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         int balance = 30;
-        int transfer = 1400;
+        int transfer = 1200;
         int condition = 1000;
-        int bonus = transfer / 100;
+        int bonus;
         if (transfer >= condition) {
+            bonus = transfer / 100;
         } else {
             bonus = 0;
         }
